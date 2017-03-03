12ed9029dbea5210c4cd76dfa8014cc61971a9a2
arrianne <arrianneoshea@hotmail.co.uk>
2017-03-03 09:30:57 +0000

    Initial commit

---
+++ config/routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const secureRoute = require('../lib/secureRoute');
+const registrations = require('../controllers/registrations');
+const sessions = require('../controllers/sessions');
+
+
+router.get('/', (req, res) => res.render('statics/index'));
+
+router.route('/register')
+.get(registrations.new)
+.post(registrations.create);
+
+router.route('/login')
+.get(sessions.new)
+.post(sessions.create);
+
+
+router.all('*', (req, res) => res.notFound());
+
+module.exports = router;
